98222e18650074d0f91f34b40fa0e8b9b81c59f2
EzraNkadi <nkadie1158@gmail.com>
2025-11-04 19:45:28 -0500

    FINALLY GOT THE ANIMATION TO WORK!

--- game.py
+++ game.py
@@ -15,20 +15,35 @@ running = True
 background = make_background()
 # TESTING ZONE ###############################
 
+current_frame = 0
+animation_speed = 0.1
+time_elapsed = 0
+
+if IDLE_FRAMES:
+    player_pos = (WIDTH//2 - IDLE_FRAMES[0].get_width(), HEIGHT//2 - IDLE_FRAMES[0].get_height())
 
 
 ########################################################################
 
 while running:
     # poll for events
+    dt = clock.tick(60)/1000
+
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    time_elapsed += dt
+    if time_elapsed >= animation_speed:
+        current_frame = (current_frame +1) % len(IDLE_FRAMES)
+        time_elapsed = 0
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
     screen.blit(background,(0,0))
-   
+    if IDLE_FRAMES:
+        current_image = IDLE_FRAMES[current_frame]
+        background.blit(current_image, player_pos)
 
 
     
--- player.py
+++ player.py
@@ -3,4 +3,25 @@ from util_params import *
 from background import *
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 #initialize idle sprite sheet
+pygame.init()
+
+
+NUM_FRAMES = 8
+IDLE_FRAMES = []
+TARGET_FRAM_WIDTH = 64
+TARGET_FRAME_HEIGHT = 64
+for i in range(NUM_FRAMES):
+
+    file_path = f'building blocks/warrior_movements/0{i}_Warrior_Idle.png'
+
+    try:
+
+        frame_image = pygame.image.load(file_path).convert_alpha()
+        scaled_frame = pygame.transform.scale(frame_image, (TARGET_FRAM_WIDTH, TARGET_FRAME_HEIGHT) )
+        IDLE_FRAMES.append(scaled_frame)
+    except pygame.error as e:
+        print(f"failed to load image")
+        print(f"pygame error")
+
+        break
 
